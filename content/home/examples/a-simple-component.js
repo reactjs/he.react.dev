2288a001d10d1980ef143f431142ec3b05523910
hannanel100 <hannanel100@gmail.com>
2022-09-08 11:10:15 +0300

    fixed error in HelloMessage component

--- content/home/examples/a-simple-component.js
+++ content/home/examples/a-simple-component.js
@@ -1,6 +1,5 @@
 class HelloMessage extends React.Component {
   render() {
-<<<<<<< HEAD
     return (
       <div>
         שלום {this.props.name}
@@ -13,10 +12,3 @@ ReactDOM.render(
   <HelloMessage name="יהונתן" />,
   document.getElementById('hello-example')
 );
-=======
-    return <div>Hello {this.props.name}</div>;
-  }
-}
-
-root.render(<HelloMessage name="Taylor" />);
->>>>>>> 84ad3308338e2bb819f4f24fa8e9dfeeffaa970b
