e1f4783c9ae992e5fa2621d7c42f26c45c3671d6
Yaron Galperin <Yaronglp@users.noreply.github.com>
2023-05-07 12:49:29 +0300

    doc: translate 'YouWillLearn' component title

--- src/components/MDX/MDXComponents.tsx
+++ src/components/MDX/MDXComponents.tsx
@@ -173,7 +173,7 @@ function YouWillLearn({
   children: any;
   isChapter?: boolean;
 }) {
-  let title = isChapter ? 'In this chapter' : 'You will learn';
+  let title = isChapter ? 'בפרק זה' : 'תוצרי למידה';
   return <SimpleCallout title={title}>{children}</SimpleCallout>;
 }
 
